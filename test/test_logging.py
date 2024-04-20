450056aa7a3272eee7cf1f2664e4352e0f1408c9
David Horsak <david.horsak@outlook.com>
2024-04-20 19:18:14 +0200

    correct exception name, store log path in constant

--- test/test_logging.py
+++ test/test_logging.py
@@ -6,6 +6,7 @@ import water_logger
 from env import ROOT_DIR
 from water_logger import getLogger
 
+LOG_PATH = os.path.join(ROOT_DIR, 'logs/app.log')
 
 def get_first_line(file_path):
     with open(file_path, "rb") as file:
@@ -14,7 +15,7 @@ def get_first_line(file_path):
 
             while file.read(1) != b'\n':
                 file.seek(-2,1)
-        except(OSERROR):
+        except(OSError):
             file.seek(0)
 
         return file.readline().decode()
@@ -25,10 +26,8 @@ def test_answer():
     
     logger = getLogger('test')
     logger.debug(unique_string)
-
-    log_path = os.path.join(ROOT_DIR, 'logs/app.log')
-    last_line = get_first_line(log_path)
+    
+    last_line = get_first_line(LOG_PATH)
 
     test_string = 'test - DEBUG - ' + str(unique_string) 
-    
     assert last_line.find(test_string) > 0
